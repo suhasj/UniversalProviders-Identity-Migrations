a40839e31bf9ee9b67ac0a3ff82922632ba10665
suhasj <suhasbjoshi@hotmail.com>
2013-11-08 15:35:01 -0800

    Legacy app ported to ASP.NET Identity

---
+++ UniversalProviders-Identity-Migrations/IdentityAccount/ProfileManagement.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UniversalProviders_Identity_Migrations.IdentityModels;
+
+namespace UniversalProviders_Identity_Migrations.Account
+{
+    public partial class ProfileManagement : System.Web.UI.Page
+    {
+        AppProfile profile = AppProfile.GetProfile();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void ProfileForm_InsertItem()
+        {
+            var item = profile.ProfileInfo;
+
+            TryUpdateModel(item);
+
+            if (ModelState.IsValid)
+            {
+                profile.Save();
+            }
+        }
+
+        // The id parameter name should match the DataKeyNames value set on the control
+        public void ProfileForm_UpdateItem(int? id)
+        {
+            var item = profile.ProfileInfo;
+
+            // Load the item here, e.g. item = MyDataLayer.Find(id);
+            if (item == null)
+            {
+                // The item wasn't found
+                ModelState.AddModelError("", String.Format("Item with id {0} was not found", id));
+                return;
+            }
+            TryUpdateModel(item);
+            if (ModelState.IsValid)
+            {
+                profile.Save();
+                // Save changes here, e.g. MyDataLayer.SaveChanges();
+
+            }
+        }
+
+        // The id parameter should match the DataKeyNames value set on the control
+        // or be decorated with a value provider attribute, e.g. [QueryString]int id
+        public UniversalProviders_Identity_Migrations.Models.ProfileInfo ProfileForm_GetItem(int? id)
+        {
+            return profile.ProfileInfo;
+        }
+    }
+}
---
+++ UniversalProviders-Identity-Migrations/Models/ProfileInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+
+namespace UniversalProviders_Identity_Migrations.Models
+{
+    [Serializable]
+    public class ProfileInfo
+    {
+        public ProfileInfo()
+        {
+            UserStats = new PersonalStats();
+        }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public PersonalStats UserStats { get; set; }
+
+        public string City { get; set; }
+    }
+
+    [Serializable]
+    public class PersonalStats
+    {
+        public int Weight { get; set; }
+
+        public int Height { get; set; }
+    }
+
+    public class AppProfile : ProfileBase
+    {
+        public ProfileInfo ProfileInfo
+        {
+            get { return (ProfileInfo)GetPropertyValue("ProfileInfo"); }
+        }
+
+        public static AppProfile GetProfile()
+        {
+            return (AppProfile)HttpContext.Current.Profile;
+        }
+
+        public static AppProfile GetProfile(string userName)
+        {
+            return (AppProfile)Create(userName);
+        }
+    }
+}
